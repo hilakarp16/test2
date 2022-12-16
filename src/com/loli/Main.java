c088fdd24dc233167de67d374ca713d35aa7e808
temp123 <temp123@Mac-mini-3.local>
2022-12-16 12:02:29 +0200

    Initial commit

---
+++ src/com/loli/Main.java
@@ -0,0 +1,24 @@
+package com.loli;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter all your grades (separated by comma):");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] grades = line.split(",");
+        int total = getTotal(grades);
+        double average = (double) total/grades.length;
+        System.out.println("The average is: " + average);
+    }
+
+    private static int getTotal(String[] grades) {
+        int result = 0;
+        for(String grade : grades) {
+            result += Integer.parseInt(grade);
+        }
+        return result;
+    }
+}
